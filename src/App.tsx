9090db6a4fe27c8e66448d56e3020732337bba49
Arturas Zuta <arturaszuta@Arturass-MacBook-Pro.local>
2022-10-09 12:56:28 -0700

    Basic setup for outline of main UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import ActionNav from './components/ActionNav';
+import Resources from './components/Resources';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='AppContainer'>
+        <div className='ResourcesContainer'>
+            <Resources
+            />
+        </div>
+        <div className='AppMainContainer'>
+          <div className='AppActionNav'>
+            <ActionNav
+            />
+          </div>
+          <div className='AppActions'>
+
+          </div>
+          <div className='AppMessages'>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Resources.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Divider from '@mui/material/Divider';
+import { Box } from '@mui/system';
+
+
+export default function Resources() {
+    return(
+        <Box> Rock: </Box>
+    )
+};
